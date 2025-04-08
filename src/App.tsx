331ed1ca355560f1b91e18ec3959f54bb6a1d0a7
naveenbutolatech <naya.butola@email.com>
2025-04-08 21:19:58 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React + Naveen</h1>
+      <h1>Vite + React + Naveen Butola</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
