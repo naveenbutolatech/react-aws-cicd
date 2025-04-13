70b1b3b6e4ad2ee839b7cc2b354eb062271dc837
naveenbutolatech <naya.butola@email.com>
2025-04-13 18:48:19 +0530

    Update App.tsx

--- src/layouts/doctor-layout/Sidebar.tsx
+++ src/layouts/doctor-layout/Sidebar.tsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
+import handleLogout from '../../utils/authHelpers';
+import { useNavigate, Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+// Define all possible menu names as a type
+type MenuName = 'create' | 'todo' | null;
 
 export default function Sidebar() {
   const [isSidebarClosed, setIsSidebarClosed] = useState(false);
-  const [openSubMenu, setOpenSubMenu] = useState(null);
+  const [openSubMenu, setOpenSubMenu] = useState<MenuName>(null);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const toggleSidebar = () => {
     setIsSidebarClosed(!isSidebarClosed);
@@ -11,7 +20,7 @@ export default function Sidebar() {
     }
   };
 
-  const toggleSubMenu = (menuName:any) => {
+  const toggleSubMenu = (menuName: MenuName) => {
     if (openSubMenu === menuName) {
       setOpenSubMenu(null);
     } else {
@@ -23,7 +32,7 @@ export default function Sidebar() {
   };
 
   return (
-    <nav id="sidebar" className={isSidebarClosed ? 'close' : ''}>
+    <nav id="sidebar">
       <ul>
         <li>
           <span className="logo">coding2go</span>
@@ -34,20 +43,20 @@ export default function Sidebar() {
           </button>
         </li>
         <li>
-          <a href="index.html">
+          <Link to="/">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
               <path d="M240-200h120v-200q0-17 11.5-28.5T400-440h160q17 0 28.5 11.5T600-400v200h120v-360L480-740 240-560v360Zm-80 0v-360q0-19 8.5-36t23.5-28l240-180q21-16 48-16t48 16l240 180q15 11 23.5 28t8.5 36v360q0 33-23.5 56.5T720-120H560q-17 0-28.5-11.5T520-160v-200h-80v200q0 17-11.5 28.5T400-120H240q-33 0-56.5-23.5T160-200Zm320-270Z"/>
             </svg>
             <span>Home</span>
-          </a>
+          </Link>
         </li>
-        <li>
-          <a href="dashboard.html">
+        <li className="active">
+          <Link to="/admin-dashboard">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
               <path d="M520-640v-160q0-17 11.5-28.5T560-840h240q17 0 28.5 11.5T840-800v160q0 17-11.5 28.5T800-600H560q-17 0-28.5-11.5T520-640ZM120-480v-320q0-17 11.5-28.5T160-840h240q17 0 28.5 11.5T440-800v320q0 17-11.5 28.5T400-440H160q-17 0-28.5-11.5T120-480Zm400 320v-320q0-17 11.5-28.5T560-520h240q17 0 28.5 11.5T840-480v320q0 17-11.5 28.5T800-120H560q-17 0-28.5-11.5T520-160Zm-400 0v-160q0-17 11.5-28.5T160-360h240q17 0 28.5 11.5T440-320v160q0 17-11.5 28.5T400-120H160q-17 0-28.5-11.5T120-160Zm80-360h160v-240H200v240Zm400 320h160v-240H600v240Zm0-480h160v-80H600v80ZM200-200h160v-80H200v80Zm160-320Zm240-160Zm0 240ZM360-280Z"/>
             </svg>
             <span>Dashboard</span>
-          </a>
+          </Link>
         </li>
         <li>
           <button 
@@ -64,9 +73,7 @@ export default function Sidebar() {
           </button>
           <ul className={`sub-menu ${openSubMenu === 'create' ? 'show' : ''}`}>
             <div>
-              <li><a href="#">Folder</a></li>
-              <li><a href="#">Document</a></li>
-              <li><a href="#">Project</a></li>
+              <li><Link to="/hospital-listing">Hospital Registration</Link></li>
             </div>
           </ul>
         </li>
@@ -75,39 +82,20 @@ export default function Sidebar() {
             className={`dropdown-btn ${openSubMenu === 'todo' ? 'rotate' : ''}`} 
             onClick={() => toggleSubMenu('todo')}
           >
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-              <path d="m221-313 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-228q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm0-320 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-548q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm339 353q-17 0-28.5-11.5T520-320q0-17 11.5-28.5T560-360h280q17 0 28.5 11.5T880-320q0 17-11.5 28.5T840-280H560Zm0-320q-17 0-28.5-11.5T520-640q0-17 11.5-28.5T560-680h280q17 0 28.5 11.5T880-640q0 17-11.5 28.5T840-600H560Z"/>
-            </svg>
-            <span>Todo-Lists</span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m370-80-16-128q-13-5-24.5-12T307-235l-119 50L78-375l103-78q-1-7-1-13.5v-27q0-6.5 1-13.5L78-585l110-190 119 50q11-8 23-15t24-12l16-128h220l16 128q13 5 24.5 12t22.5 15l119-50 110 190-103 78q1 7 1 13.5v27q0 6.5-2 13.5l103 78-110 190-118-50q-11 8-23 15t-24 12L590-80H370Zm70-80h79l14-106q31-8 57.5-23.5T639-327l99 41 39-68-86-65q5-14 7-29.5t2-31.5q0-16-2-31.5t-7-29.5l86-65-39-68-99 42q-22-23-48.5-38.5T533-694l-13-106h-79l-14 106q-31 8-57.5 23.5T321-633l-99-41-39 68 86 64q-5 15-7 30t-2 32q0 16 2 31t7 30l-86 65 39 68 99-42q22 23 48.5 38.5T427-266l13 106Zm42-180q58 0 99-41t41-99q0-58-41-99t-99-41q-59 0-99.5 41T342-480q0 58 40.5 99t99.5 41Zm-2-140Z"/></svg>
+            <span>Setting</span>
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
               <path d="M480-361q-8 0-15-2.5t-13-8.5L268-556q-11-11-11-28t11-28q11-11 28-11t28 11l156 156 156-156q11-11 28-11t28 11q11 11 11 28t-11 28L508-372q-6 6-13 8.5t-15 2.5Z"/>
             </svg>
           </button>
           <ul className={`sub-menu ${openSubMenu === 'todo' ? 'show' : ''}`}>
             <div>
-              <li><a href="#">Work</a></li>
-              <li><a href="#">Private</a></li>
-              <li><a href="#">Coding</a></li>
-              <li><a href="#">Gardening</a></li>
-              <li><a href="#">School</a></li>
+              <li><a href="#" onClick={() => handleLogout(dispatch, navigate)}>Logout</a></li>
+              <li><Link to="#">Profile Settings</Link></li>
+              <li><Link to="#">Hospital Settings</Link></li>
+              <li><Link to="#">System Preferences</Link></li>
             </div>
           </ul>
-        </li>
-        <li className="active">
-          <a href="calendar.html">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-              <path d="M200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-40q0-17 11.5-28.5T280-880q17 0 28.5 11.5T320-840v40h320v-40q0-17 11.5-28.5T680-880q17 0 28.5 11.5T720-840v40h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Zm0-480h560v-80H200v80Zm0 0v-80 80Zm280 240q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240Z"/>
-            </svg>
-            <span>Calendar</span>
-          </a>
-        </li>
-        <li>
-          <a href="profile.html">
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed">
-              <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-240v-32q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v32q0 33-23.5 56.5T720-160H240q-33 0-56.5-23.5T160-240Zm80 0h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z"/>
-            </svg>
-            <span>Profile</span>
-          </a>
         </li>
       </ul>
     </nav>
